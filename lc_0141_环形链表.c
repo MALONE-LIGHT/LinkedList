be790cf26d68251ce3565f51228467b0d8a59cad
MALONE-LIGHT <38933435+MALONE-LIGHT@users.noreply.github.com>
2021-03-06 19:41:11 +0800

    Add files via upload

---
+++ lc_0141_环形链表.c
@@ -0,0 +1,68 @@
+/*
+给定一个链表，判断链表中是否有环。
+
+如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+
+如果链表中存在环，则返回 true 。 否则，返回 false 。
+
+ 
+
+进阶：
+
+你能用 O(1)（即，常量）内存解决此问题吗？
+
+ 
+
+示例 1：
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+示例 2：
+输入：head = [1,2], pos = 0
+输出：true
+解释：链表中有一个环，其尾部连接到第一个节点。
+
+示例 3：
+输入：head = [1], pos = -1
+输出：false
+解释：链表中没有环。
+
+*/
+
+/*
+思路：
+通过快慢指针来判断是否有环
+1.慢指针位于链头，快指针位于链头下一节点。（因为循环结束条件是判断快慢指针是否重合相等，若都在链头那么循环不会开始）
+2.慢指针每次走一步，快指针每次走两步。
+3.判断快慢指针是否会重合，直到快指针走到链尾。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head)
+{
+    if(NULL == head || NULL == head->next)
+    {
+        return false;
+    }
+
+    struct ListNode* slow = head;
+    struct ListNode* fast = head->next;
+    while(slow != fast)
+    {
+        if(NULL == fast || NULL == fast->next)
+        {
+            return false;
+        }
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    return true;
+}
